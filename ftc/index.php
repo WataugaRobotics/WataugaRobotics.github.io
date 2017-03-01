7afcb629c2e3843c36643c07281e8f9090860da7
benbroce3 <benbroce2@gmail.com>
2017-02-28 22:08:17 -0500

    add subsite folders & redirect for 5881

---
+++ ftc/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>redirect...</title>
+	</head>
+	<body>
+		<?php header('Location: https://jacobcw00.wixsite.com/wataugarobotics5881'); exit; ?>
+	</body>
+</html>
